3c72e87ed58be62050e988c25bf8ca0309e371a4
AphroNyade <adelyza.desumeur@ig2i.centralelille.fr>
2022-06-16 19:00:48 +0200

    Improved the CSS

--- function.js
+++ function.js
@@ -2,31 +2,41 @@ function search() {
     
 }
 
+let lfl = 'lightflashlight', dfl = 'darkflashlight';
+let lsb = 'lightsearchbutton', dsb = 'darksearchbutton';
+let hdr = 'headertitle', lbg = '#FFD673', dbg = '#030303';
+let ldp = 'lightdisplay', ddp = 'darkdisplay';
+let lcr = '#FFFFFF', dcr = '#030303';
 
-function changeLightMode(lamp1, lamp2) {
-    let lightsearch = 'lightsearchbutton', darksearch = 'darksearchbutton';
-    let currentLight = document.getElementById(lamp1);
-    let otherLight = document.getElementById(lamp2);
-
-    if (currentLight.src.includes('darkmode')){
-            console.log('From dark to light');
-            currentLight.style.display = 'none';
-            otherLight.style.display = 'inline';
-            changeLens(darksearch, lightsearch);
-            changeHeader('headertitle');
-            changeBackground('#E5EBEA');
-            changeBorder('#030303');
-    } else {
-            console.log('From light to dark');
-            currentLight.style.display = 'none';
-            otherLight.style.display = 'inline';
-            changeLens(lightsearch, darksearch);
-            changeHeader('headertitle');
-            changeBackground('#030303');
-            changeBorder('#E5EBEA');
-    }
+function lightToDark() {
+    console.log('From light to dark');
+    changeFlashlight(lfl, dfl);
+    changeLens(lsb, dsb);
+    changeColor(lcr);
+    changeHeader(hdr);
+    changeBackground(dbg);
+    changeBorder(lcr);
+    changeDisplay(ldp, ddp);
 }
 
+function darkToLight() {
+    console.log('From dark to light');
+    changeFlashlight(dfl, lfl);
+    changeLens(dsb, lsb);
+    changeColor(dcr);
+    changeHeader(hdr);
+    changeBackground(lbg);
+    changeBorder(dbg);
+    changeDisplay(ddp, ldp);
+} 
+
+function changeFlashlight(flashlight1, flashlight2) {
+    currentFlashight = document.getElementById(flashlight1);
+    otherFlashlight = document.getElementById(flashlight2);
+
+    currentFlashight.style.display = 'none';
+    otherFlashlight.style.display = 'inline';
+} 
 
 function changeLens(lens1, lens2) {
     let currentLens = document.getElementById(lens1);
@@ -34,6 +44,13 @@ function changeLens(lens1, lens2) {
 
     currentLens.style.display = 'none';
     otherLens.style.display = 'inline';
+} 
+
+function changeColor(Color) {
+    let fontList = document.getElementsByTagName('p');
+    for (para of fontList) para.style.color = Color;
+    fontList = document.getElementsByTagName('li');
+    for (para of fontList) para.style.color = Color;
 }
 
 function changeHeader(headertitle) {
@@ -44,13 +61,22 @@ function changeHeader(headertitle) {
         headerLight.className = 'lightmode';
         headerLight.style.borderBottomColor = '#030303';
     }
-}
+} 
 
 function changeBackground(hex) {
     document.body.style.backgroundColor = hex;
-}
+} 
 
 function changeBorder(borderBottomColor) {
     let headerBorder = document.getElementById('header');
     headerBorder.style.borderBottomColor = borderBottomColor;
 }
+
+function changeDisplay(dis1, dis2) {
+    let currentDisplay = document.getElementById(dis1);
+    let otherDisplay = document.getElementById(dis2);
+
+    currentDisplay.style.display = 'none';
+    otherDisplay.style.display = 'inline';
+}
+
